bb636b72b62e4ec2d2af157f5778edcb59533bb5
JCP <info@amtx.de>
2026-04-15 12:07:28 +0200

    Updated test.php with new content on 2026-04-15.

--- test.php
+++ test.php
@@ -1,3 +1,12 @@
 <?php
-echo "Test file";
+$filename = 'codelines.php';
+$lines = file($filename);
+
+// Display the lines
+foreach ($lines as $line_num => $line) {
+    echo "Line " . ($line_num + 1) . ": " . htmlspecialchars($line);
+}
+
+// Or just get the array
+print_r($lines);
 ?>
